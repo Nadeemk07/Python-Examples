a6e2aeb68714dd0555b49316833cf82cba0faae4
NADEEM KAISAR <eznizer@gmail.com>
2021-03-29 13:36:28 +0530

    Add files via upload

---
+++ Arrays/reverse.py
@@ -0,0 +1,7 @@
+def reverse_string():
+    word = input("enter the string")
+    word = word[::-1]
+    print(word)
+
+
+reverse_string()
---
+++ Arrays/sort.py
@@ -0,0 +1,10 @@
+def combine_array():
+    arr1 = [1, 2, 3]
+    arr2 = [4, 5, 1]
+    final_array = arr1+arr2
+    print(final_array)
+    a = final_array.sort()
+    print(a)
+
+
+combine_array()
